d644e1175994cd69a17e461fa66b1f17afe23ca1
bohao huang <hbhzhuce@gmail.com>
2017-04-30 19:27:30 +0000

    bh 04/30: added setup files

---
+++ setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "sta-663",
+    version = "1.0.0",
+    author = "Bohao Huang, Siyang Yuan",
+    author_email = "bohao.huang@duke.edu, siyang.yuan@duke.edu",
+    description = ("A class project of STA663, implementation of Variational Autoencoder"),
+    license = "MIT",
+    keywords = "Variational Autoencoder",
+    url = "https://github.com/qingshuimonk/STA663.git",
+    packages=['vae'],
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+    ],
+)
